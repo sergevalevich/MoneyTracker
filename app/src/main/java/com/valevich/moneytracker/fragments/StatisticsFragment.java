05e2a53aec3fb699ec5b30127837ade56f74f004
Sergey Valevich <romsolog@mail.ru>
2016-04-20 16:37:44 +0300

    Created Statistics Fragment

---
+++ app/src/main/java/com/valevich/moneytracker/fragments/StatisticsFragment.java
@@ -0,0 +1,26 @@
+package com.valevich.moneytracker.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.valevich.moneytracker.R;
+
+public class StatisticsFragment extends Fragment {
+
+    public StatisticsFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_statistics, container, false);
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+}
