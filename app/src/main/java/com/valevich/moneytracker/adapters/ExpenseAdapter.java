0127ff3d410fd7e3639ee42cfea962c8c2c267ee
Sergey Valevich <romsolog@mail.ru>
2016-04-20 12:57:52 +0300

    created expense recyclerview adapter

---
+++ app/src/main/java/com/valevich/moneytracker/adapters/ExpenseAdapter.java
@@ -0,0 +1,59 @@
+package com.valevich.moneytracker.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.valevich.moneytracker.R;
+import com.valevich.moneytracker.model.Expense;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder> {
+
+    private List<Expense> mExpenses;
+
+    public ExpenseAdapter (List<Expense> expenses) {
+        mExpenses = expenses;
+    }
+
+    @Override
+    public ExpenseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.expense_list_item,parent,false);
+        return new ExpenseViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ExpenseViewHolder holder, int position) {
+        holder.bindExpense(mExpenses.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExpenses.size();
+    }
+
+    class ExpenseViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.price)
+        TextView price;
+        @Bind(R.id.description)
+        TextView description;
+
+        public ExpenseViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+
+        public void bindExpense(Expense expense) {
+            price.setText(expense.getPrice());
+            description.setText(expense.getDescription());
+        }
+    }
+}
