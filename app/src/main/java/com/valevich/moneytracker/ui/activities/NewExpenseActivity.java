9fc70b887856135fcda6b93dc0ec4f9d6079c96f
Sergey Valevich <romsolog@mail.ru>
2016-05-01 08:38:50 +0300

    created empty activity and layout file to add new expense

---
+++ app/src/main/java/com/valevich/moneytracker/ui/activities/NewExpenseActivity.java
@@ -0,0 +1,20 @@
+package com.valevich.moneytracker.ui.activities;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.valevich.moneytracker.R;
+
+
+
+public class NewExpenseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_expense);
+    }
+
+}
