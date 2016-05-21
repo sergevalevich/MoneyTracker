a8cacc1f0d877d2ef69b36b7827e4a29aeaa8b30
Sergey Valevich <romsolog@mail.ru>
2016-05-21 07:30:38 +0300

    implemented SignUp Activity layout

---
+++ app/src/main/java/com/valevich/moneytracker/ui/activities/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.valevich.moneytracker.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.valevich.moneytracker.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
