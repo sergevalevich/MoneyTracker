0ed42758371134b13caafe3c16439e7deb3a14aa
Sergey Valevich <romsolog@mail.ru>
2016-05-22 08:29:16 +0300

    injected ConnectivityManager

--- app/src/main/java/com/valevich/moneytracker/utils/NetworkStatusChecker.java
+++ app/src/main/java/com/valevich/moneytracker/utils/NetworkStatusChecker.java
@@ -1,5 +1,6 @@
 package com.valevich.moneytracker.utils;
 
+import android.app.NotificationManager;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -8,6 +9,7 @@ import com.valevich.moneytracker.ui.activities.SignUpActivity;
 
 import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.RootContext;
+import org.androidannotations.annotations.SystemService;
 
 /**
  * Created by NotePad.by on 20.05.2016.
@@ -16,10 +18,10 @@ import org.androidannotations.annotations.RootContext;
 public class NetworkStatusChecker {
     @RootContext
     Context mContext;
+    @SystemService
+    ConnectivityManager mConnectivityManager;
     public boolean isNetworkAvailable() {
-        ConnectivityManager cm =
-                (ConnectivityManager) mContext.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        NetworkInfo activeNetwork = mConnectivityManager.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 }
